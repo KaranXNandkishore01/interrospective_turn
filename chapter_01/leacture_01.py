5572827ba87dd0812c446ae9e099ade43658de31
Karan Prajapati <karanpraja6@gmail.com>
2025-02-28 17:43:08 +0530

    added python files and text file

---
+++ chapter_01/leacture_01.py
@@ -0,0 +1,30 @@
+# a = 123
+# b = 456
+# c = a + b
+# print(c)
+
+# print("My name is Karan.")
+# print(456546564 + 456456456)
+
+# a = 23
+# b = 12
+# C = a + b
+# d = 12.212021
+# e = 'karan'
+# print(C)
+
+# print(type(a))
+# print(type(b))
+# print(type(C))
+# print(type(d))
+# print(type(e))
+ 
+ 
+name1 = 'karan'
+name2 = "karan"
+name3 = '''karan'''
+name4 = """karan"""
+print(name1)
+print(name2)
+print(name3)
+print(name4)
